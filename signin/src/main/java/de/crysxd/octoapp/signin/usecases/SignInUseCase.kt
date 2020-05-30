8591c04d903f263f8ab6459fc27a9d8b032982b7
Christian Würthner <c.wurthner@gmail.com>
2020-05-30 16:02:49 +0200

    Let SignInUseCase return success or error

--- signin/src/main/java/de/crysxd/octoapp/signin/usecases/SignInUseCase.kt
+++ signin/src/main/java/de/crysxd/octoapp/signin/usecases/SignInUseCase.kt
@@ -7,11 +7,13 @@ import de.crysxd.octoapp.signin.models.SignInInformation
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import timber.log.Timber
+import java.lang.Exception
 
 
-class SignInUseCase(private val octoprintRepository: OctoPrintRepository) : UseCase<SignInInformation, Unit> {
+class SignInUseCase(private val octoprintRepository: OctoPrintRepository) :
+    UseCase<SignInInformation, Boolean> {
 
-    override suspend fun execute(param: SignInInformation) = withContext(Dispatchers.IO) {
+    override suspend fun execute(param: SignInInformation): Boolean = withContext(Dispatchers.IO) {
         val octoprintInstanceInformation = OctoPrintInstanceInformation(
             param.ipAddress,
             param.port.toInt(),
@@ -20,10 +22,15 @@ class SignInUseCase(private val octoprintRepository: OctoPrintRepository) : UseC
 
         val octoprint = octoprintRepository.getOctoprint(octoprintInstanceInformation)
 
-        // Test connection, will throw in case of faulty configuration
-        val version = octoprint.createVersionApi().getVersion()
-        Timber.i("Connected to ${version.serverVersionText}")
-
-        octoprintRepository.storeOctoprintInstanceInformation(octoprintInstanceInformation)
+        try {
+            // Test connection, will throw in case of faulty configuration
+            val version = octoprint.createVersionApi().getVersion()
+            Timber.i("Connected to ${version.serverVersionText}")
+            octoprintRepository.storeOctoprintInstanceInformation(octoprintInstanceInformation)
+            true
+        } catch (e: Exception) {
+            Timber.e(e)
+            false
+        }
     }
 }
