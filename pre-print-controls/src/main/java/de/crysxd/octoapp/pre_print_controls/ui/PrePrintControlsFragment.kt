c1a7c5fc011e338f19522ae316d5234090673abd
Christian Würthner <c.wurthner@gmail.com>
2020-05-31 14:45:35 +0200

    Add new module, move to module when printer gets connected

---
+++ pre-print-controls/src/main/java/de/crysxd/octoapp/pre_print_controls/ui/PrePrintControlsFragment.kt
@@ -0,0 +1,6 @@
+package de.crysxd.octoapp.pre_print_controls.ui
+
+import androidx.fragment.app.Fragment
+import de.crysxd.octoapp.pre_print_controls.R
+
+class PrePrintControlsFragment : Fragment(R.layout.fragment_pre_print_controls)
