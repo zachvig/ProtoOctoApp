720c4c42a3a3ba1a5e71e0f469050778ecc8f36f
Christian Würthner <c.wurthner@gmail.com>
2020-06-21 10:42:33 +0200

    Style QR scanner

---
+++ signin/src/main/java/de/crysxd/octoapp/signin/ui/ScannerView.kt
@@ -0,0 +1,15 @@
+package de.crysxd.octoapp.signin.ui
+
+import android.content.Context
+import android.graphics.Color
+import android.util.AttributeSet
+import me.dm7.barcodescanner.core.ViewFinderView
+import me.dm7.barcodescanner.zxing.ZXingScannerView
+
+class ScannerView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) : ZXingScannerView(context, attrs) {
+
+    override fun createViewFinderView(context: Context) = ViewFinderView(context).also {
+        it.setBorderColor(Color.WHITE)
+        it.setLaserColor(Color.TRANSPARENT)
+    }
+}
