87a7fbbbb7b3bd940ebfda1f3d14d6cd87f4b0ac
Christian Würthner <c.wurthner@gmail.com>
2020-06-30 21:41:41 +0200

    Fix wrong injector used

--- print-controls/src/main/java/de/crysxd/octoapp/print_controls/di/ViewModelInjectionHelpers.kt
+++ print-controls/src/main/java/de/crysxd/octoapp/print_controls/di/ViewModelInjectionHelpers.kt
@@ -19,7 +19,7 @@ inline fun <reified VM : ViewModel> FragmentActivity.injectViewModel(provider: V
 }
 
 
-internal inline fun <reified VM : ViewModel> OctoWidget.injectViewModel(provider: ViewModelProvider.Factory = de.crysxd.octoapp.base.di.Injector.get().viewModelFactory()) = lazy {
+internal inline fun <reified VM : ViewModel> OctoWidget.injectViewModel(provider: ViewModelProvider.Factory = Injector.get().viewModelFactory()) = lazy {
     val vm = ViewModelProvider(this.parent, provider)[VM::class.java]
     (vm as? BaseViewModel)?.let { this.setupBaseViewModel(it) }
     vm
