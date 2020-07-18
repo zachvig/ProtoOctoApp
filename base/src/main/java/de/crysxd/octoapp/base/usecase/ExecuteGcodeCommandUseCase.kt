dc643d1e5d384dfd1b4a6a6fa5afc75afeb6c85e
Christian Würthner <c.wurthner@gmail.com>
2020-07-18 14:53:51 +0200

    Log event when gcode is sent

--- base/src/main/java/de/crysxd/octoapp/base/usecase/ExecuteGcodeCommandUseCase.kt
+++ base/src/main/java/de/crysxd/octoapp/base/usecase/ExecuteGcodeCommandUseCase.kt
@@ -1,5 +1,8 @@
 package de.crysxd.octoapp.base.usecase
 
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.ktx.logEvent
+import com.google.firebase.ktx.Firebase
 import de.crysxd.octoapp.octoprint.OctoPrint
 import de.crysxd.octoapp.octoprint.models.printer.GcodeCommand
 import timber.log.Timber
@@ -9,6 +12,18 @@ class ExecuteGcodeCommandUseCase @Inject constructor() : UseCase<Pair<OctoPrint,
 
     override suspend fun execute(param: Pair<OctoPrint, GcodeCommand>) {
         Timber.i("Executing: ${param.second}")
+
+        when (param.second) {
+            is GcodeCommand.Single -> logEvent((param.second as GcodeCommand.Single).command)
+            is GcodeCommand.Batch -> (param.second as GcodeCommand.Batch).commands.forEach { logEvent(it) }
+        }
+
         param.first.createPrinterApi().executeGcodeCommand(param.second)
+    }
+
+    private fun logEvent(command: String) {
+        Firebase.analytics.logEvent("gcode_send") {
+            param("command", command)
+        }
     }
 }
