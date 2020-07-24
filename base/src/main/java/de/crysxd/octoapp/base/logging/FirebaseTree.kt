1a91cb3dc41ac976490a87c9b117068e327a2eb3
Christian Würthner <c.wurthner@gmail.com>
2020-07-24 17:47:11 +0200

    Only log info and up

--- base/src/main/java/de/crysxd/octoapp/base/logging/FirebaseTree.kt
+++ base/src/main/java/de/crysxd/octoapp/base/logging/FirebaseTree.kt
@@ -1,12 +1,15 @@
 package de.crysxd.octoapp.base.logging
 
+import android.util.Log
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import timber.log.Timber
 
 class FirebaseTree : Timber.DebugTree() {
 
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-        FirebaseCrashlytics.getInstance().log("$tag | $message")
-        t?.let { FirebaseCrashlytics.getInstance().recordException(it) }
+        if (priority >= Log.INFO) {
+            FirebaseCrashlytics.getInstance().log("$tag | $message")
+            t?.let { FirebaseCrashlytics.getInstance().recordException(it) }
+        }
     }
 }
