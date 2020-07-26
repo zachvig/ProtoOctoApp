4c908e599f0b13a016e365d8e8e46b2033fd8ab8
Christian Würthner <c.wurthner@gmail.com>
2020-07-26 10:57:52 +0200

    Hide nav bar

--- base/src/main/java/de/crysxd/octoapp/base/ui/widget/webcam/FullscreenWebcamActivity.kt
+++ base/src/main/java/de/crysxd/octoapp/base/ui/widget/webcam/FullscreenWebcamActivity.kt
@@ -1,6 +1,7 @@
 package de.crysxd.octoapp.base.ui.widget.webcam
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import de.crysxd.octoapp.base.R
 
@@ -9,5 +10,10 @@ class FullscreenWebcamActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fullscreen_webcam)
+
+        window.decorView.apply {
+            // Hide both the navigation bar and the status bar.
+            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
     }
 }
