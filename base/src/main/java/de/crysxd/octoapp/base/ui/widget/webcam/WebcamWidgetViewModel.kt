d889902a708c5a2e8aa20088f512808745031068
Christian Würthner <c.wurthner@gmail.com>
2020-07-29 20:42:07 +0200

    Reload camera feed when settings are saved

--- base/src/main/java/de/crysxd/octoapp/base/ui/widget/webcam/WebcamWidgetViewModel.kt
+++ base/src/main/java/de/crysxd/octoapp/base/ui/widget/webcam/WebcamWidgetViewModel.kt
@@ -7,8 +7,10 @@ import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.liveData
 import androidx.lifecycle.map
 import de.crysxd.octoapp.base.OctoPrintProvider
+import de.crysxd.octoapp.base.livedata.OctoTransformations.filterEventsForMessageType
 import de.crysxd.octoapp.base.ui.BaseViewModel
 import de.crysxd.octoapp.octoprint.models.settings.WebcamSettings
+import de.crysxd.octoapp.octoprint.models.socket.Message
 import timber.log.Timber
 
 
@@ -20,9 +22,12 @@ class WebcamWidgetViewModel(
     private var previousSource: LiveData<UiState>? = null
     private val uiStateMediator = MediatorLiveData<UiState>()
     val uiState = uiStateMediator.map { it }
+    private val settingsUpdatedLiveData = octoPrintProvider.eventLiveData
+        .filterEventsForMessageType(Message.EventMessage.SettingsUpdated::class.java)
 
     init {
         uiStateMediator.addSource(octoPrintProvider.octoPrint) { connect() }
+        uiStateMediator.addSource(settingsUpdatedLiveData) { connect() }
         uiStateMediator.postValue(UiState.Loading)
         connect()
     }
