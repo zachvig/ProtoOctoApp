fb85398457c22a7413f5a219fdcfe8c6f2f713d5
Christian Würthner <c.wurthner@gmail.com>
2020-07-29 21:06:19 +0200

    Fix user not returned to app

--- base/src/main/java/de/crysxd/octoapp/base/ui/widget/webcam/FullscreenWebcamActivity.kt
+++ base/src/main/java/de/crysxd/octoapp/base/ui/widget/webcam/FullscreenWebcamActivity.kt
@@ -1,11 +1,22 @@
 package de.crysxd.octoapp.base.ui.widget.webcam
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import de.crysxd.octoapp.base.R
 
+
 class FullscreenWebcamActivity : AppCompatActivity() {
+
+    companion object {
+        fun start(activity: Activity) {
+            // For some reason when navigating back after leaving the app, we do not go back to the previous activity
+            // so the user left the app accidentally. By using startActivityForResult we prevent this
+            activity.startActivityForResult(Intent(activity, FullscreenWebcamActivity::class.java), 23343)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
