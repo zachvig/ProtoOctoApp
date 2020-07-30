c08606c808f378ec4e52c6e5536ef3df6572226c
Christian Würthner <c.wurthner@gmail.com>
2020-07-30 21:24:48 +0200

    Style bottom menu and dialogs

--- base/src/main/java/de/crysxd/octoapp/base/ui/common/MenuBottomSheet.kt
+++ base/src/main/java/de/crysxd/octoapp/base/ui/common/MenuBottomSheet.kt
@@ -7,6 +7,7 @@ import android.view.*
 import android.widget.PopupMenu
 import androidx.annotation.IdRes
 import androidx.annotation.MenuRes
+import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.lifecycleScope
@@ -21,12 +22,14 @@ import kotlinx.android.synthetic.main.item_menu.view.*
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
+
 abstract class MenuBottomSheet : BottomSheetDialogFragment() {
 
     private lateinit var adapter: MenuAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = RecyclerView(requireContext())
+        view.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.window_background))
         view.layoutManager = LinearLayoutManager(requireContext())
         adapter = MenuAdapter(requireContext(), getMenuRes()) {
             lifecycleScope.launch {
