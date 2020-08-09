8aad561bef2c900890ea63603baf0350715f64c9
Christian Würthner <c.wurthner@gmail.com>
2020-08-09 14:22:44 +0200

    Collect body of repsonses with unexpected code

--- octoprint/src/main/java/de/crysxd/octoapp/octoprint/exceptions/OctoPrintApiException.kt
+++ octoprint/src/main/java/de/crysxd/octoapp/octoprint/exceptions/OctoPrintApiException.kt
@@ -2,4 +2,5 @@ package de.crysxd.octoapp.octoprint.exceptions
 
 import okhttp3.HttpUrl
 
-class OctoPrintApiException(httpUrl: HttpUrl, responseCode: Int) : OctoPrintException(message = "Received unexpected response code $responseCode from $httpUrl")
+class OctoPrintApiException(httpUrl: HttpUrl, responseCode: Int, body: String) :
+    OctoPrintException(message = "Received unexpected response code $responseCode from $httpUrl (body=$body)")
