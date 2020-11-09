a36f734775d94bb4e12522a4bbe5f3023ee72b54
Christian Würthner <c.wurthner@gmail.com>
2020-11-09 16:16:26 +0100

    Fix casting issue

--- base/src/main/java/de/crysxd/octoapp/base/ext/FlowExt.kt
+++ base/src/main/java/de/crysxd/octoapp/base/ext/FlowExt.kt
@@ -3,9 +3,12 @@ package de.crysxd.octoapp.base.ext
 import de.crysxd.octoapp.octoprint.models.socket.Event
 import de.crysxd.octoapp.octoprint.models.socket.Message
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.mapNotNull
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.map
 
 @Suppress("UNCHECKED_CAST")
-fun <T : Message> Flow<Event>.filterEventsForMessageType(): Flow<T> = mapNotNull {
-    ((it as? Event.MessageReceived)?.message) as? T
+inline fun <reified T : Message> Flow<Event>.filterEventsForMessageType(): Flow<T> = filter {
+    it is Event.MessageReceived && T::class.java.isAssignableFrom(it.message::class.java)
+}.map {
+    (it as Event.MessageReceived).message as T
 }
