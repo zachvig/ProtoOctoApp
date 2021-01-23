114c37ecf9106a98d82bde8e9f93c723d97499a9
Christian Würthner <c.wurthner@gmail.com>
2021-01-23 17:08:11 +0100

    Suppress Tradfri 500 errors

--- base/src/main/java/de/crysxd/octoapp/base/usecase/CyclePsuUseCase.kt
+++ base/src/main/java/de/crysxd/octoapp/base/usecase/CyclePsuUseCase.kt
@@ -2,6 +2,7 @@ package de.crysxd.octoapp.base.usecase
 
 import de.crysxd.octoapp.base.OctoAnalytics
 import de.crysxd.octoapp.octoprint.plugins.power.PowerDevice
+import de.crysxd.octoapp.octoprint.plugins.power.tradfri.runWithTradfriFix
 import kotlinx.coroutines.delay
 import timber.log.Timber
 import javax.inject.Inject
@@ -10,16 +11,12 @@ class CyclePsuUseCase @Inject constructor() : UseCase<PowerDevice, Unit>() {
 
     override suspend fun doExecute(param: PowerDevice, timber: Timber.Tree) {
         OctoAnalytics.logEvent(OctoAnalytics.Event.PsuCycled)
-        val e = try {
+        runWithTradfriFix(param) {
             param.turnOff()
-            null
-        } catch (e: Exception) {
-            timber.e(e, "Error while turning off, continuing anyways")
-            e
         }
         delay(2000)
-        param.turnOn()
-        delay(1000)
-        e?.let { throw it }
+        runWithTradfriFix(param) {
+            param.turnOn()
+        }
     }
 }
