451b87c0fb854395c5ade1c9d67c9a6fb60b47fa
Christian Würthner <c.wurthner@gmail.com>
2021-01-23 10:02:29 +0100

    Pin feedback by default

--- base/src/main/java/de/crysxd/octoapp/base/repository/PinnedMenuItemRepository.kt
+++ base/src/main/java/de/crysxd/octoapp/base/repository/PinnedMenuItemRepository.kt
@@ -3,6 +3,7 @@ package de.crysxd.octoapp.base.repository
 import de.crysxd.octoapp.base.datasource.DataSource
 import de.crysxd.octoapp.base.ui.common.menu.main.MENU_ITEM_CANCEL_PRINT
 import de.crysxd.octoapp.base.ui.common.menu.main.MENU_ITEM_EMERGENCY_STOP
+import de.crysxd.octoapp.base.ui.common.menu.main.MENU_ITEM_SEND_FEEDBACK
 import de.crysxd.octoapp.base.ui.common.menu.main.MENU_ITEM_TURN_PSU_OFF
 
 
@@ -10,7 +11,7 @@ class PinnedMenuItemRepository(
     private val dataSource: DataSource<Set<String>>
 ) {
 
-    private val defaults = setOf(MENU_ITEM_CANCEL_PRINT, MENU_ITEM_EMERGENCY_STOP, MENU_ITEM_TURN_PSU_OFF)
+    private val defaults = setOf(MENU_ITEM_SEND_FEEDBACK, MENU_ITEM_CANCEL_PRINT, MENU_ITEM_EMERGENCY_STOP, MENU_ITEM_TURN_PSU_OFF)
 
     fun toggleMenuItemPinned(itemId: String) {
         val data = (dataSource.get() ?: defaults).toMutableList()
