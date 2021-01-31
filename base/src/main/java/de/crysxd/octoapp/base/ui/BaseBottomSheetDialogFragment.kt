e38acd2b1e6c7f8970b3fafa4a0bfa298c26c5ca
Christian Würthner <c.wurthner@gmail.com>
2021-01-31 10:45:28 +0100

    Fix bottom sheet on tablets

--- base/src/main/java/de/crysxd/octoapp/base/ui/BaseBottomSheetDialogFragment.kt
+++ base/src/main/java/de/crysxd/octoapp/base/ui/BaseBottomSheetDialogFragment.kt
@@ -1,14 +1,23 @@
 package de.crysxd.octoapp.base.ui
 
+import android.content.Context
 import android.os.Build
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.annotation.CallSuper
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.doOnLayout
 import androidx.core.view.updatePadding
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import de.crysxd.octoapp.base.R
+import de.crysxd.octoapp.base.ui.ext.findParent
 import de.crysxd.octoapp.base.ui.ext.requireOctoActivity
+
 
 abstract class BaseBottomSheetDialogFragment : BottomSheetDialogFragment() {
 
@@ -42,5 +51,28 @@ abstract class BaseBottomSheetDialogFragment : BottomSheetDialogFragment() {
                 bottom = (activity?.window?.decorView?.rootWindowInsets?.systemWindowInsetBottom ?: 0)
             )
         }
+
+        // Fix bottom sheet not fully shown on tablet in landscape
+        val coordinator = requireView().findParent<CoordinatorLayout>()
+        coordinator?.findViewById<View>(R.id.design_bottom_sheet)?.let { bottomSheet ->
+            val behaviour = BottomSheetBehavior.from(bottomSheet)
+            bottomSheet.doOnLayout {
+                behaviour.peekHeight = bottomSheet.height
+                coordinator.parent.requestLayout()
+            }
+        }
+
+        // Limit bottom sheet width to get a pleasant look on tablets
+
+        val maxWidth = requireContext().resources.getDimension(R.dimen.max_bottom_sheet_width).toInt()
+        dialog?.window?.setLayout(getScreenWidth().coerceAtMost(maxWidth), ViewGroup.LayoutParams.MATCH_PARENT)
+    }
+
+    protected fun getScreenWidth(): Int {
+        val wm = requireContext().getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val display = wm.defaultDisplay
+        val metrics = DisplayMetrics()
+        display.getMetrics(metrics)
+        return metrics.widthPixels
     }
 }
