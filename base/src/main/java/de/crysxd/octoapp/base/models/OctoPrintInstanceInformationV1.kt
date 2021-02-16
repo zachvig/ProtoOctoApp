b70617729db36d9a48a3e7a708a69a78f594576b
Christian Würthner <c.wurthner@gmail.com>
2021-02-16 07:34:39 +0100

    Do not show user info in labels

--- base/src/main/java/de/crysxd/octoapp/base/models/OctoPrintInstanceInformationV1.kt
+++ base/src/main/java/de/crysxd/octoapp/base/models/OctoPrintInstanceInformationV1.kt
@@ -2,6 +2,7 @@ package de.crysxd.octoapp.base.models
 
 import de.crysxd.octoapp.octoprint.models.settings.Settings
 import de.crysxd.octoapp.octoprint.models.system.SystemCommand
+import kotlin.math.max
 
 private const val M115_MASK = "{m155 response}"
 
@@ -32,7 +33,12 @@ data class OctoPrintInstanceInformationV2(
 
     val isWebcamSupported get() = settings?.webcam?.webcamEnabled == true
 
-    val label get() = settings?.appearance?.name ?: webUrl.replace("https://", "").replace("http://", "")
+    val label
+        get() = settings?.appearance?.name?.takeIf { it.isNotBlank() } ?: webUrl.let {
+            val protocolEnd = it.indexOf("://") + 1
+            val userInfoEnd = it.indexOf("@") + 1
+            it.substring(max(protocolEnd, userInfoEnd))
+        }
 
     // We do not want to log the M115 response all over the place. It clutters the logs.
     override fun toString(): String = if (m115Response != null && m115Response != M115_MASK) {
