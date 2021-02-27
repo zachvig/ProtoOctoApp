9640d080804822ea673051eef535e4bbe573bcca
Christian Würthner <c.wurthner@gmail.com>
2021-02-27 14:57:53 +0100

    Remove UseCaseModule

--- signin/src/main/java/de/crysxd/octoapp/signin/di/SignInComponent.kt
+++ signin/src/main/java/de/crysxd/octoapp/signin/di/SignInComponent.kt
@@ -2,13 +2,12 @@ package de.crysxd.octoapp.signin.di
 
 import androidx.lifecycle.ViewModelProvider
 import dagger.Component
-import de.crysxd.octoapp.base.repository.OctoPrintRepository
 import de.crysxd.octoapp.base.di.BaseComponent
+import de.crysxd.octoapp.base.repository.OctoPrintRepository
 
 @SignInScope
 @Component(
     modules = [
-        UseCaseModule::class,
         ViewModelModule::class
     ],
     dependencies = [
