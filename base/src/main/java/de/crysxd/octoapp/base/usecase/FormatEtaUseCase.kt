2424344e4a469d7e0a86d7109dec568283895112
Christian Würthner <c.wurthner@gmail.com>
2021-03-01 16:45:53 +0100

    Enforce device locale to be used for time formats

--- base/src/main/java/de/crysxd/octoapp/base/usecase/FormatEtaUseCase.kt
+++ base/src/main/java/de/crysxd/octoapp/base/usecase/FormatEtaUseCase.kt
@@ -1,6 +1,8 @@
 package de.crysxd.octoapp.base.usecase
 
 import android.content.Context
+import android.content.res.Resources
+import androidx.core.os.ConfigurationCompat
 import de.crysxd.octoapp.base.R
 import timber.log.Timber
 import java.text.DateFormat
@@ -21,10 +23,21 @@ class FormatEtaUseCase @Inject constructor(
     override suspend fun doExecute(param: Params, timber: Timber.Tree): String {
         val eta = Date(System.currentTimeMillis() + TimeUnit.SECONDS.toMillis(param.secsLeft))
         val lessThanAnHour = TimeUnit.SECONDS.toMinutes(param.secsLeft) < 60
+
+        // We enforece the usage of the device language for time formatting; user might use OctoApp in English
+        // but does for sure not want AM/PM
+        val locale = try {
+            val deviceLanguage = ConfigurationCompat.getLocales(Resources.getSystem().configuration)[0].language
+            Locale.forLanguageTag(deviceLanguage)
+        } catch (e: Exception) {
+            // Just precautionary try/catch
+            Locale.getDefault()
+        }
+
         return when {
             lessThanAnHour && param.allowRelative -> context.getString(R.string.x_left, formatDurationUseCase.execute(param.secsLeft))
-            eta.isToday() -> context.getString(R.string.eta_x, DateFormat.getTimeInstance(DateFormat.SHORT).format(eta))
-            else -> context.getString(R.string.eta_x, DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT).format(eta))
+            eta.isToday() -> context.getString(R.string.eta_x, DateFormat.getTimeInstance(DateFormat.SHORT, locale).format(eta))
+            else -> context.getString(R.string.eta_x, DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT, locale).format(eta))
         }
     }
 
