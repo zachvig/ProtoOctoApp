b387257a19f5e52fef332e84153ce9fc3a9db2ba
Christian Würthner <c.wurthner@gmail.com>
2021-03-01 19:39:49 +0100

    Fiux delay for progress to show up when opening the app

--- base/src/main/java/de/crysxd/octoapp/base/ext/FlowExt.kt
+++ base/src/main/java/de/crysxd/octoapp/base/ext/FlowExt.kt
@@ -12,3 +12,12 @@ inline fun <reified T : Message> Flow<Event>.filterEventsForMessageType(): Flow<
 }.map {
     (it as Event.MessageReceived).message as T
 }
+
+fun <T> Flow<T>.smartSample(sampleRateMs: Long): Flow<T> {
+    var last = 0L
+    return this.filter {
+        val pass = (System.currentTimeMillis() - last) > sampleRateMs
+        last = System.currentTimeMillis()
+        pass
+    }
+}
--- print-controls/src/main/java/de/crysxd/octoapp/print_controls/ui/widget/progress/ProgressWidgetViewModel.kt
+++ print-controls/src/main/java/de/crysxd/octoapp/print_controls/ui/widget/progress/ProgressWidgetViewModel.kt
@@ -4,18 +4,18 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import de.crysxd.octoapp.base.OctoPrintProvider
 import de.crysxd.octoapp.base.ext.filterEventsForMessageType
+import de.crysxd.octoapp.base.ext.smartSample
 import de.crysxd.octoapp.octoprint.models.socket.Message
 import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.sample
 
 class ProgressWidgetViewModel(
     octoPrintProvider: OctoPrintProvider
 ) : ViewModel() {
 
     val printState = octoPrintProvider.eventFlow("progress_widget")
-        .sample(5000)
         .filterEventsForMessageType<Message.CurrentMessage>()
         .filter { it.progress != null }
+        .smartSample(5000)
         .asLiveData()
 
 }
