8bd50a21602c2563242d08f434512d6b32fdaa22
Christian Würthner <c.wurthner@gmail.com>
2021-03-05 17:24:21 +0100

    Fix links not opening on API 30

--- base/src/main/java/de/crysxd/octoapp/base/ext/UriExt.kt
+++ base/src/main/java/de/crysxd/octoapp/base/ext/UriExt.kt
@@ -1,17 +1,19 @@
 package de.crysxd.octoapp.base.ext
 
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.widget.Toast
 import timber.log.Timber
 
+
 fun Uri.open(context: Context) {
     Timber.i("Opening link: $this")
     val intent = Intent(Intent.ACTION_VIEW, this).also { it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK) }
-    if (context.packageManager.resolveActivity(intent, 0) != null) {
+    try {
         context.startActivity(intent)
-    } else {
+    } catch (e: ActivityNotFoundException) {
         Toast.makeText(context, "Unable to open link, no app found", Toast.LENGTH_SHORT).show()
     }
 }
