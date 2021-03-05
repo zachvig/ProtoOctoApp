73bd372515352cc3acd9269d74e85966c6f801df
Christian Würthner <c.wurthner@gmail.com>
2021-03-05 17:09:29 +0100

    Add log about links being opened

--- base/src/main/java/de/crysxd/octoapp/base/ext/UriExt.kt
+++ base/src/main/java/de/crysxd/octoapp/base/ext/UriExt.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.widget.Toast
+import timber.log.Timber
 
 fun Uri.open(context: Context) {
+    Timber.i("Opening link: $this")
     val intent = Intent(Intent.ACTION_VIEW, this).also { it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK) }
     if (context.packageManager.resolveActivity(intent, 0) != null) {
         context.startActivity(intent)
