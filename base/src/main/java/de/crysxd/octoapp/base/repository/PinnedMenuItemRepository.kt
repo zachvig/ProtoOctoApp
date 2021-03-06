ea4b44468804206ac1aacfcae1d566a02e1e427b
Christian Würthner <c.wurthner@gmail.com>
2021-03-06 20:26:13 +0100

    Pin open octoprint by default

--- base/src/main/java/de/crysxd/octoapp/base/repository/PinnedMenuItemRepository.kt
+++ base/src/main/java/de/crysxd/octoapp/base/repository/PinnedMenuItemRepository.kt
@@ -1,17 +1,14 @@
 package de.crysxd.octoapp.base.repository
 
 import de.crysxd.octoapp.base.datasource.DataSource
-import de.crysxd.octoapp.base.ui.menu.main.MENU_ITEM_CANCEL_PRINT
-import de.crysxd.octoapp.base.ui.menu.main.MENU_ITEM_EMERGENCY_STOP
-import de.crysxd.octoapp.base.ui.menu.main.MENU_ITEM_HELP
-import de.crysxd.octoapp.base.ui.menu.main.MENU_ITEM_TURN_PSU_OFF
+import de.crysxd.octoapp.base.ui.menu.main.*
 
 
 class PinnedMenuItemRepository(
     private val dataSource: DataSource<Set<String>>
 ) {
 
-    private val defaults = setOf(MENU_ITEM_CANCEL_PRINT, MENU_ITEM_EMERGENCY_STOP, MENU_ITEM_TURN_PSU_OFF, MENU_ITEM_HELP)
+    private val defaults = setOf(MENU_ITEM_OPEN_OCTOPRINT, MENU_ITEM_CANCEL_PRINT, MENU_ITEM_EMERGENCY_STOP, MENU_ITEM_TURN_PSU_OFF, MENU_ITEM_HELP)
 
     fun toggleMenuItemPinned(itemId: String) {
         val data = (dataSource.get() ?: defaults).toMutableList()
