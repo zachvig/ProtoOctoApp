39319961482e971d5a8d9f5be993d3a3437773b5
Christian Würthner <c.wurthner@gmail.com>
2021-03-06 16:08:20 +0100

    Cancel polling livedat

--- base/src/main/java/de/crysxd/octoapp/base/livedata/PollingLiveData.kt
+++ base/src/main/java/de/crysxd/octoapp/base/livedata/PollingLiveData.kt
@@ -2,30 +2,32 @@ package de.crysxd.octoapp.base.livedata
 
 import android.os.Handler
 import androidx.lifecycle.LiveData
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import timber.log.Timber
+import java.lang.Runnable
 
 class PollingLiveData<T>(
     private val interval: Long = 1000,
     private val action: suspend () -> T
 ) : LiveData<PollingLiveData.Result<T>>() {
 
+    private var job = Job()
     private val handler = Handler()
     private val runnable = Runnable { poll() }
 
     override fun onActive() {
         super.onActive()
+        job = Job()
         poll()
     }
 
     override fun onInactive() {
         super.onInactive()
+        job.cancel()
         handler.removeCallbacks(runnable)
     }
 
-    private fun poll(): Job = GlobalScope.launch {
+    private fun poll(): Job = GlobalScope.launch(job + Dispatchers.IO) {
         try {
             postValue(Result.Success(action()))
         } catch (e: Exception) {
