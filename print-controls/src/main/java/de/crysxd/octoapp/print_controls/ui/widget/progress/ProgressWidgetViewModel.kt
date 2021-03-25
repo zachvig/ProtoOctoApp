dff7a66249f854f51a8d66a0ea4133cafeeb655d
Christian Würthner <c.wurthner@gmail.com>
2021-03-25 09:11:00 +0700

    Update logs

--- print-controls/src/main/java/de/crysxd/octoapp/print_controls/ui/widget/progress/ProgressWidgetViewModel.kt
+++ print-controls/src/main/java/de/crysxd/octoapp/print_controls/ui/widget/progress/ProgressWidgetViewModel.kt
@@ -15,7 +15,6 @@ class ProgressWidgetViewModel(
     val printState = octoPrintProvider.passiveCurrentMessageFlow("progress_widget")
         .filter { it.progress != null }
         .rateLimit(5000)
-        .onEach { Timber.i("Pass") }
         .asLiveData()
 
 }
