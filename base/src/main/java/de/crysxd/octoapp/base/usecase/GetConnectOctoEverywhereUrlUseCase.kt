55caf3908eca340292cddc4de30062e27b1d9803
Christian Würthner <c.wurthner@gmail.com>
2021-04-12 09:39:17 +0700

    Add a fallback for old OE plugins

--- base/src/main/java/de/crysxd/octoapp/base/usecase/GetConnectOctoEverywhereUrlUseCase.kt
+++ base/src/main/java/de/crysxd/octoapp/base/usecase/GetConnectOctoEverywhereUrlUseCase.kt
@@ -7,8 +7,10 @@ import de.crysxd.octoapp.base.OctoAnalytics
 import de.crysxd.octoapp.base.OctoPrintProvider
 import de.crysxd.octoapp.base.R
 import de.crysxd.octoapp.base.di.Injector
+import de.crysxd.octoapp.octoprint.models.settings.Settings
 import timber.log.Timber
 import java.lang.Exception
+import java.lang.IllegalStateException
 import javax.inject.Inject
 
 
@@ -19,11 +21,29 @@ class GetConnectOctoEverywhereUrlUseCase @Inject constructor(
 ) : UseCase<Unit, GetConnectOctoEverywhereUrlUseCase.Result>() {
 
     override suspend fun doExecute(param: Unit, timber: Timber.Tree) = try {
-        val printerId = octoPrintProvider.octoPrint().createOctoEverywhereApi().getInfo().printerId
+        val printerId = try {
+            octoPrintProvider.octoPrint().createOctoEverywhereApi().getInfo().printerId
+        } catch (e: Exception) {
+            Timber.w(e)
+            null
+        } ?: try {
+            octoPrintProvider.octoPrint().createSettingsApi()
+                .getSettings()
+                .plugins.values
+                .mapNotNull { it as? Settings.OctoEverywhere }
+                .firstOrNull()
+                ?.printerKey
+        } catch (e: Exception) {
+            Timber.w(e)
+            null
+        } ?: throw IllegalStateException("OctoEverywhere not installed")
+
         OctoAnalytics.logEvent(OctoAnalytics.Event.OctoEverywhereConnectStarted)
-        Result.Success(Firebase.remoteConfig.getString("octoeverywhere_app_portal_url")
-            .replace("{{{printerid}}}", printerId)
-            .replace("{{{callbackPath}}}", OCTOEVERYWHERE_APP_PORTAL_CALLBACK_PATH))
+        Result.Success(
+            Firebase.remoteConfig.getString("octoeverywhere_app_portal_url")
+                .replace("{{{printerid}}}", printerId)
+                .replace("{{{callbackPath}}}", OCTOEVERYWHERE_APP_PORTAL_CALLBACK_PATH)
+        )
     } catch (e: Exception) {
         OctoAnalytics.logEvent(OctoAnalytics.Event.OctoEverywherePluginMissing)
         Result.Error(Injector.get().localizedContext().getString(R.string.configure_remote_acces___octoeverywhere___error_install_plugin), e)
