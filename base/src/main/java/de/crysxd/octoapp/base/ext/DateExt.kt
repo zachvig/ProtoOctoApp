fb4959e646c511b1e123ede8e4daa00d11642751
Christian Würthner <c.wurthner@gmail.com>
2021-05-11 09:23:15 +0700

    Update ETA readability

--- base/src/main/java/de/crysxd/octoapp/base/ext/DateExt.kt
+++ base/src/main/java/de/crysxd/octoapp/base/ext/DateExt.kt
@@ -1,11 +1,15 @@
 package de.crysxd.octoapp.base.ext
 
 import android.content.res.Resources
+import android.text.format.DateUtils
 import androidx.core.os.ConfigurationCompat
+import de.crysxd.octoapp.base.di.Injector
 import timber.log.Timber
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
+
+private const val dateFlags = DateUtils.FORMAT_SHOW_DATE or DateUtils.FORMAT_ABBREV_ALL or DateUtils.FORMAT_SHOW_TIME or DateUtils.FORMAT_NUMERIC_DATE
 
 // We can't use Locale.getDefault() because we overwrite the app language but we _always_ want to use the user's local preferences for time
 private fun getDeviceLocale() = try {
@@ -17,13 +21,18 @@ private fun getDeviceLocale() = try {
     Locale.getDefault()
 }
 
-fun Date.format(forceShowDate: Boolean = false): String = if (isToday() && !forceShowDate) {
-    DateFormat.getTimeInstance(DateFormat.SHORT, getDeviceLocale()).format(this)
-} else {
-    DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT, getDeviceLocale()).format(this)
+fun Date.format(forceShowDate: Boolean = false): String = when {
+    isToday() && !forceShowDate -> DateFormat.getTimeInstance(DateFormat.SHORT, getDeviceLocale()).format(this)
+    isThisYear() -> DateUtils.formatDateTime(Injector.get().context(), time, dateFlags or DateUtils.FORMAT_NO_YEAR)
+    else -> DateUtils.formatDateTime(Injector.get().context(), time, dateFlags)
 }
 
 fun Date.isToday(): Boolean {
     val format = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+    return format.format(Date()) == format.format(this)
+}
+
+fun Date.isThisYear(): Boolean {
+    val format = SimpleDateFormat("yyyy", Locale.ENGLISH)
     return format.format(Date()) == format.format(this)
 }
--- print-controls/src/main/java/de/crysxd/octoapp/print_controls/ui/widget/progress/ProgressWidget.kt
+++ print-controls/src/main/java/de/crysxd/octoapp/print_controls/ui/widget/progress/ProgressWidget.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.content.ContextCompat
+import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
@@ -26,7 +27,7 @@ import de.crysxd.octoapp.octoprint.models.socket.Message
 import de.crysxd.octoapp.print_controls.R
 import de.crysxd.octoapp.print_controls.databinding.ProgressWidgetBinding
 import de.crysxd.octoapp.print_controls.di.injectViewModel
-import timber.log.Timber
+import kotlinx.coroutines.delay
 import kotlin.math.roundToInt
 
 class ProgressWidget(context: Context) : RecyclableOctoWidget<ProgressWidgetBinding, ProgressWidgetViewModel>(context) {
@@ -36,9 +37,23 @@ class ProgressWidget(context: Context) : RecyclableOctoWidget<ProgressWidgetBind
     private var lastProgress: Float? = null
     override val binding = ProgressWidgetBinding.inflate(LayoutInflater.from(context))
     private val observer = Observer(::updateView)
+    private var first = true
+    private val progressPercentLayoutThreshold = 80f
 
-    override fun createNewViewModel(parent: BaseWidgetHostFragment) =
-        parent.injectViewModel<ProgressWidgetViewModel>().value
+    override fun createNewViewModel(parent: BaseWidgetHostFragment): ProgressWidgetViewModel {
+        binding.textViewEtaLabel.isInvisible = true
+        binding.textVieEta.isInvisible = true
+        binding.estimationIndicator.isInvisible = true
+        binding.textViewTimeSpentLabel.isInvisible = true
+        binding.textViewTimeSpent.isInvisible = true
+        binding.textViewTimeLeftLabel.isInvisible = true
+        binding.textViewTimeLeft.isInvisible = true
+        binding.textViewPrintName.isInvisible = true
+        binding.textViewPrintNameLabel.isInvisible = true
+        binding.textViewProgressPercent.isInvisible = true
+        first = true
+        return parent.injectViewModel<ProgressWidgetViewModel>().value
+    }
 
     override fun getTitle(context: Context) = context.getString(R.string.progress)
     override fun getAnalyticsName() = "progress"
@@ -56,46 +71,30 @@ class ProgressWidget(context: Context) : RecyclableOctoWidget<ProgressWidgetBind
     private fun updateView(message: Message.CurrentMessage) {
         parent.lifecycleScope.launchWhenStarted {
             val progressPercent = message.progress?.completion ?: 0f
-            val progressPercentLayoutThreshold = 80f
             val progress = progressPercent.toInt() / 100f
             val printTimeLeft = message.progress?.printTimeLeft?.takeIf { it != 0 }?.toLong()
             val printTimeSpent = message.progress?.printTime?.takeIf { it != 0 }?.toLong()
+            val loading = progressPercent == 0f && printTimeLeft == null && printTimeSpent == null
+            val formattedSpent = (printTimeSpent ?: 0L).takeIf { printTimeLeft != null }?.let { formatDuration(it) }
+            val formattedLeft = printTimeLeft?.let { formatDuration(it) }
+            val formattedEta = printTimeLeft?.let { formatEta(it) }
             val progressText = when {
                 message.state?.flags?.cancelling == true -> context.getString(R.string.cancelling)
-                progressPercent == 0f && printTimeLeft == null && printTimeSpent == null -> context.getString(R.string.loading)
+                loading -> context.getString(R.string.loading)
                 else -> context.getString(R.string.x_percent, progress * 100f)
             }
 
-            if (lastProgress != progress) {
+            if (first) {
+                // Show intro animation :)
+                delay(500)
+                first = false
+            }
+
+            if (lastProgress != progress || first) {
                 TransitionManager.beginDelayedTransition(binding.root)
             }
 
-            ConstraintSet().apply {
-                clone(binding.root as ConstraintLayout)
-                constrainPercentWidth(R.id.progressBar, progress)
-                clear(R.id.textViewProgressPercent, ConstraintSet.END)
-                clear(R.id.textViewProgressPercent, ConstraintSet.START)
-                connect(
-                    R.id.textViewProgressPercent,
-                    if (progressPercent > progressPercentLayoutThreshold) {
-                        ConstraintSet.END
-                    } else {
-                        ConstraintSet.START
-                    },
-                    R.id.progressBar,
-                    ConstraintSet.END
-                )
-                connect(
-                    R.id.textViewProgressPercent,
-                    if (progressPercent > progressPercentLayoutThreshold) {
-                        ConstraintSet.START
-                    } else {
-                        ConstraintSet.END
-                    },
-                    R.id.parent,
-                    ConstraintSet.END
-                )
-            }.applyTo(binding.root)
+            updateProgressBar(progress, progressPercent)
 
             binding.textViewProgressPercent.setTextColor(
                 ContextCompat.getColor(
@@ -111,22 +110,61 @@ class ProgressWidget(context: Context) : RecyclableOctoWidget<ProgressWidgetBind
                 ObjectAnimator.ofInt(this, "level", level, (10000f * progress).roundToInt()).start()
             }
 
+            if (loading) {
+                return@launchWhenStarted
+            }
+
             binding.progressBarFill.backgroundTintList = ColorStateList.valueOf(ColorTheme.activeColorTheme.dark)
             binding.textViewProgressPercent.text = progressText
             binding.textViewPrintName.text = message.job?.file?.display
-            binding.textViewTimeSpent.text = (printTimeSpent ?: 0L).takeIf { printTimeLeft != null }?.let { formatDuration(it) }
-            binding.textViewTimeLeft.text = printTimeLeft?.let { formatDuration(it) }
-            binding.textVieEta.text = printTimeLeft?.let { formatEta(it) }
+            binding.textViewTimeSpent.text = formattedSpent
+            binding.textViewTimeLeft.text = formattedLeft
+            binding.textVieEta.text = formattedEta
             binding.estimationIndicator.isVisible = printTimeLeft != null
             binding.estimationIndicator.setImageResource(message.progress?.printTimeLeftOrigin.asPrintTimeLeftImageResource())
             binding.estimationIndicator.setColorFilter(ContextCompat.getColor(context, message.progress?.printTimeLeftOrigin.asPrintTimeLeftOriginColor()))
-            binding.textViewProgressPercent.isVisible = true
-            binding.textViewPrintName.isVisible = true
-            binding.textViewTimeSpent.isVisible = true
-            binding.textViewTimeLeft.isVisible = true
+            binding.textViewEtaLabel.isVisible = true
             binding.textVieEta.isVisible = true
+            binding.estimationIndicator.isVisible = true
+            binding.textViewTimeSpentLabel.isVisible = true
+            binding.textViewTimeSpent.isVisible = true
+            binding.textViewPrintName.isVisible = true
+            binding.textViewPrintNameLabel.isVisible = true
+            binding.textViewProgressPercent.isVisible = true
+            binding.textViewTimeLeftLabel.isVisible = true
+            binding.textViewTimeLeft.isVisible = true
             lastProgress = progress
         }
+    }
+
+    private fun updateProgressBar(progress: Float, progressPercent: Float) {
+        ConstraintSet().apply {
+            clone(binding.root as ConstraintLayout)
+            constrainPercentWidth(R.id.progressBar, progress)
+            clear(R.id.textViewProgressPercent, ConstraintSet.END)
+            clear(R.id.textViewProgressPercent, ConstraintSet.START)
+            connect(
+                R.id.textViewProgressPercent,
+                if (progressPercent > progressPercentLayoutThreshold) {
+                    ConstraintSet.END
+                } else {
+                    ConstraintSet.START
+                },
+                R.id.progressBar,
+                ConstraintSet.END
+            )
+            connect(
+                R.id.textViewProgressPercent,
+                if (progressPercent > progressPercentLayoutThreshold) {
+                    ConstraintSet.START
+                } else {
+                    ConstraintSet.END
+                },
+                R.id.parent,
+                ConstraintSet.END
+            )
+        }.applyTo(binding.root)
+
     }
 
     private suspend fun formatDuration(seconds: Long) = formatDurationUseCase.execute(seconds)
