4810d405fec97a859c896b70396b5d682f703a53
Christian Würthner <c.wurthner@gmail.com>
2021-05-14 10:44:28 +0700

    #612 Only apply heated bed and chamber when profile has such a component

--- base/src/main/java/de/crysxd/octoapp/base/usecase/SetTargetTemperaturesUseCase.kt
+++ base/src/main/java/de/crysxd/octoapp/base/usecase/SetTargetTemperaturesUseCase.kt
@@ -8,10 +8,12 @@ import timber.log.Timber
 import javax.inject.Inject
 
 class SetTargetTemperaturesUseCase @Inject constructor(
-    private val octoPrintProvider: OctoPrintProvider
+    private val octoPrintProvider: OctoPrintProvider,
+    private val getCurrentPrinterProfileUseCase: GetCurrentPrinterProfileUseCase,
 ) : UseCase<SetTargetTemperaturesUseCase.Params, Unit>() {
 
     override suspend fun doExecute(param: Params, timber: Timber.Tree) {
+        val profile = getCurrentPrinterProfileUseCase.execute(Unit)
         param.temps.filter { it.temperature != null }.forEach {
             when {
                 it.component.startsWith("tool") -> {
@@ -27,13 +29,13 @@ class SetTargetTemperaturesUseCase @Inject constructor(
                     )
                 }
 
-                it.component == "bed" -> {
+                it.component == "bed" && profile.heatedBed -> {
                     octoPrintProvider.octoPrint().createPrinterApi().executeBedCommand(
                         BedCommand.SetTargetTemperature(it.temperature ?: 0)
                     )
                 }
 
-                it.component == "chamber" -> {
+                it.component == "chamber" && profile.heatedChamber -> {
                     octoPrintProvider.octoPrint().createPrinterApi().executeChamberCommand(
                         ChamberCommand.SetTargetTemperature(it.temperature ?: 0)
                     )
