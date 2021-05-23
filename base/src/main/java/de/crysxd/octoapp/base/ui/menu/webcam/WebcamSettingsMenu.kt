cb290ecc63bcfb161d898492bbd9cc52e4989695
Christian Würthner <c.wurthner@gmail.com>
2021-05-23 17:39:36 +0700

    #623 Make resolution option more prominent

--- base/src/main/java/de/crysxd/octoapp/base/ui/menu/webcam/WebcamSettingsMenu.kt
+++ base/src/main/java/de/crysxd/octoapp/base/ui/menu/webcam/WebcamSettingsMenu.kt
@@ -46,9 +46,10 @@ class WebcamSettingsMenu : Menu {
         private val maxResolution = (FirebaseRemoteConfig.getInstance().getLong("free_webcam_max_resolution") * (1080 / 1920f)).roundToInt()
         override val itemId = MENU_ITEM_ENABLE_FULL_WEBCAM_RESOLUTION
         override var groupId = ""
-        override val order = 161
-        override val style = MenuItemStyle.Settings
-        override val icon = R.drawable.ic_round_photo_size_select_large_24
+        override val order = 1
+        override val canBePinned = false
+        override val style = MenuItemStyle.Support
+        override val icon = R.drawable.ic_round_favorite_24
         override suspend fun isVisible(destinationId: Int) = !BillingManager.isFeatureEnabled(BillingManager.FEATURE_FULL_WEBCAM_RESOLUTION)
 
         override suspend fun getTitle(context: Context) = context.getString(R.string.webcam_settings___enable_full_resolution)
