604f233d198ca4bba3b7a6cf24d9b5044f1919ca
Christian Würthner <c.wurthner@gmail.com>
2021-07-02 19:35:01 +0200

    Do not reuqire activity to show an error message, may cause crash

--- base/src/main/java/de/crysxd/octoapp/base/ui/ext/FragmentExt.kt
+++ base/src/main/java/de/crysxd/octoapp/base/ui/ext/FragmentExt.kt
@@ -5,3 +5,5 @@ import de.crysxd.octoapp.base.ui.base.OctoActivity
 
 fun Fragment.requireOctoActivity(): OctoActivity = requireActivity() as? OctoActivity
     ?: throw IllegalStateException("Fragment is not hosted by a OctoActivity")
+
+fun Fragment.optionallyRequestOctoActivity(): OctoActivity? = requireActivity() as? OctoActivity
