e5e6397e768b947be6239f0db89718d95240489e
Christian Würthner <c.wurthner@gmail.com>
2021-07-10 08:29:59 +0200

    Make sure local DNS fails fast

--- base/src/main/java/de/crysxd/octoapp/base/dns/LocalDnsResolver.kt
+++ base/src/main/java/de/crysxd/octoapp/base/dns/LocalDnsResolver.kt
@@ -8,6 +8,7 @@ import com.qiniu.android.dns.local.AndroidDnsServer
 import com.qiniu.android.dns.local.Resolver
 import de.crysxd.octoapp.base.OctoAnalytics
 import de.crysxd.octoapp.base.utils.measureTime
+import kotlinx.coroutines.*
 import timber.log.Timber
 import java.net.InetAddress
 import java.net.UnknownHostException
@@ -20,6 +21,7 @@ class LocalDnsResolver(private val context: Context) {
     companion object {
         private val cache = mutableListOf<DnsEntry>()
         private const val CACHE_ENTRY_TTL = 600_000L
+        private const val RESOLVE_TIMEOUT = 3
         private val resolveLock = ReentrantLock()
     }
 
@@ -34,49 +36,55 @@ class LocalDnsResolver(private val context: Context) {
             return@withLock it.resolvedIp
         }
 
+        // Resolve. We do this in a coroutine scope and execute the resolve async so we can limit the time effectively
         measureTime("local_dns_resolve") {
-            // From the WiFi manager, get the gateway and DHCP server address, also replace last octet of own ip address with 1
-            // Both usually is the router
-            // For good measure, also add common router IPs in case the user has a double DHCP issue
-            val wifi = context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-            val dnsIps = listOf(
-                wifi.dhcpInfo.gateway.asIpString(),
-                wifi.dhcpInfo.serverAddress.asIpString(),
-                wifi.dhcpInfo.ipAddress.asIpString().split(".").toMutableList().also { it[3] = "1" }.joinToString("."),
-//                "192.168.0.1",
-//                "192.168.1.1",
-//                "192.168.2.1",
-            ).distinct()
-
-            Timber.i("Using as DNS server: $dnsIps")
-
-            // Use the standard Android resolver as fallback
-            // Add all DNS server from above
-            val resolvers = listOf(
-                listOf(AndroidDnsServer.defaultResolver(context)),
-                dnsIps.map { Resolver(InetAddress.getByName(it)) }
-            ).flatten().toTypedArray()
-
-            // Resolve!
-            val res = try {
-                val dns = DnsManager(NetworkInfo.normal, resolvers)
-                dns.query(hostName).firstOrNull() ?: throw UnknownHostException("Resolving $hostName resulted in null")
-            } catch (e: Exception) {
-                Timber.e(e, "Unable to resolve host $hostName")
-                throw UnknownHostException(hostName)
-            }
-
-            Timber.i("Resolved ${hostName}=$res")
-            OctoAnalytics.logEvent(OctoAnalytics.Event.InbuiltDnsResolveSuccess)
-            cache.add(
-                DnsEntry(
-                    hostName = hostName,
-                    resolvedIp = res,
-                    validUntil = Date(System.currentTimeMillis() + CACHE_ENTRY_TTL)
-                )
-            )
-            return res
+            doResolve(hostName)
         }
+    }
+
+    private fun doResolve(hostName: String): String {
+        // From the WiFi manager, get the gateway and DHCP server address, also replace last octet of own ip address with 1
+        // Both usually is the router
+        // For good measure, also add common router IPs in case the user has a double DHCP issue
+        val wifi = context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        val dnsIps = listOf(
+            wifi.dhcpInfo.gateway.asIpString(),
+            wifi.dhcpInfo.serverAddress.asIpString(),
+            wifi.dhcpInfo.ipAddress.asIpString().split(".").toMutableList().also { it[3] = "1" }.joinToString("."),
+            "192.168.0.1",
+            "192.168.1.1",
+            "192.168.2.1",
+        ).distinct()
+
+        Timber.i("Using as DNS server: $dnsIps")
+
+        // Use the standard Android resolver as fallback
+        // Add all DNS server from above
+        val resolvers = listOf(
+            listOf(AndroidDnsServer.defaultResolver(context)),
+            dnsIps.map { Resolver(InetAddress.getByName(it), RESOLVE_TIMEOUT) }
+        ).flatten().toTypedArray()
+
+        // Resolve!
+        val res = try {
+            val dns = DnsManager(NetworkInfo.normal, resolvers)
+            dns.queryErrorHandler = DnsManager.QueryErrorHandler { e, host -> Timber.w("Unable to resolve host $host (${e::class.java.simpleName}: ${e.message})") }
+            dns.query(hostName).firstOrNull() ?: throw UnknownHostException("Resolving $hostName resulted in null")
+        } catch (e: Exception) {
+            Timber.e(e, "Unable to resolve host $hostName")
+            throw UnknownHostException(hostName)
+        }
+
+        Timber.i("Resolved ${hostName}=$res")
+        OctoAnalytics.logEvent(OctoAnalytics.Event.InbuiltDnsResolveSuccess)
+        cache.add(
+            DnsEntry(
+                hostName = hostName,
+                resolvedIp = res,
+                validUntil = Date(System.currentTimeMillis() + CACHE_ENTRY_TTL)
+            )
+        )
+        return res
     }
 
     private fun Int.asIpString(): String {
