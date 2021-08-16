20db499e503f8ac89d1f2b68dadd8608c4018dfe
Christian Würthner <c.wurthner@gmail.com>
2021-08-16 08:37:59 +0200

    #764 Allow Tradfri plugin to accept Int and String as id

--- octoprint/src/main/java/de/crysxd/octoapp/octoprint/plugins/power/tradfri/TradfriPowerDevice.kt
+++ octoprint/src/main/java/de/crysxd/octoapp/octoprint/plugins/power/tradfri/TradfriPowerDevice.kt
@@ -4,14 +4,23 @@ import com.google.gson.annotations.SerializedName
 import de.crysxd.octoapp.octoprint.plugins.power.PowerDevice
 
 data class TradfriPowerDevice(
-    @SerializedName("id") val idInt: Int,
+    // The plugin gives back a Int or a String as id depending on its mood...we get it as any and clean it up below
+    @SerializedName("id") val idAny: Any,
     @Transient val plugin: TradfriPowerPlugin?,
     @SerializedName("name") override val displayName: String,
 ) : PowerDevice() {
     override val capabilities
         get() = listOf(Capability.ControlPrinterPower, Capability.Illuminate)
 
-    override val id: String get() = idInt.toString()
+    override val id: String
+        get() = when (idAny) {
+            is Number -> idAny.toInt().toString()
+            is String -> idAny
+            else -> throw java.lang.IllegalStateException("idAny is $idAny")
+        }
+
+    // We need to copy ourselves for serialization to make sure we have a string in the idAny field... 🙄
+    fun asSerializableVersion() = copy(idAny = id)
 
     @Transient
     override val pluginDisplayName = "Trådfri"
