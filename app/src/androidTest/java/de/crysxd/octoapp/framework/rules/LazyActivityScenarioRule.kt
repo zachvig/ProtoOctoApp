ea1a831803ffc655a5de14a85173ff95ecbd8d57
Christian Würthner <c.wurthner@gmail.com>
2021-08-21 09:52:58 +0200

    Collect logs of test runs

--- app/src/androidTest/java/de/crysxd/octoapp/framework/rules/LazyActivityScenarioRule.kt
+++ app/src/androidTest/java/de/crysxd/octoapp/framework/rules/LazyActivityScenarioRule.kt
@@ -11,6 +11,7 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.runner.screenshot.Screenshot
 import de.crysxd.octoapp.BuildConfig
+import de.crysxd.octoapp.base.di.Injector
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 import timber.log.Timber
@@ -37,9 +38,13 @@ open class LazyActivityScenarioRule<A : Activity>(private val launchActivity: Bo
     override fun failed(e: Throwable?, description: Description) {
         super.failed(e, description)
         val bitmap = Screenshot.capture().bitmap
-        val file = getScreenshotFile(description)
-        Timber.i("Writing screenshot to ${file.absolutePath}")
-        bitmap.compress(Bitmap.CompressFormat.WEBP_LOSSY, 80, file.outputStream())
+        val screenshotFile = getFile(description, "webp")
+        Timber.i("Writing screenshot to ${screenshotFile.absolutePath}")
+        bitmap.compress(Bitmap.CompressFormat.WEBP_LOSSY, 80, screenshotFile.outputStream())
+        val logsFile = getFile(description, "log")
+        Timber.i("Writing logs to ${logsFile.absolutePath}")
+        logsFile.writeText(Injector.get().timberCacheTree().logs)
+
         scenario?.close()
     }
 
@@ -49,6 +54,11 @@ open class LazyActivityScenarioRule<A : Activity>(private val launchActivity: Bo
     }
 
     fun launch(newIntent: Intent? = null) {
+        Injector.get().timberCacheTree().also {
+            it.collectVerbose = true
+            it.clear()
+        }
+
         if ((scenario?.state ?: Lifecycle.State.INITIALIZED) >= Lifecycle.State.CREATED) throw IllegalStateException("Scenario has already been launched!")
         newIntent?.let { scenarioSupplier = { ActivityScenario.launch(it) } }
         scenario = scenarioSupplier()
@@ -56,11 +66,11 @@ open class LazyActivityScenarioRule<A : Activity>(private val launchActivity: Bo
 
     fun getScenario(): ActivityScenario<A> = checkNotNull(scenario)
 
-    private fun getScreenshotFile(description: Description): File {
+    private fun getFile(description: Description, suffix: String): File {
         val context = InstrumentationRegistry.getInstrumentation().targetContext
         val dir = File(BuildConfig.FAILED_TEST_SCREENSHOT_DIR)
         if (!dir.exists()) dir.mkdirs()
-        val filename = "${description.testName}.webp"
+        val filename = "${description.testName}.$suffix"
         return File(dir, filename)
     }
 
