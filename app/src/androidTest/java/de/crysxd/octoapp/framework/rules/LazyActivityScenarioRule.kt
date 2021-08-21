aaa39785e1148530696a42f311593c95ea3435e5
Christian Würthner <c.wurthner@gmail.com>
2021-08-21 09:06:39 +0200

    Take screenshots on test failure

--- app/src/androidTest/java/de/crysxd/octoapp/framework/rules/LazyActivityScenarioRule.kt
+++ app/src/androidTest/java/de/crysxd/octoapp/framework/rules/LazyActivityScenarioRule.kt
@@ -2,60 +2,67 @@ package de.crysxd.octoapp.framework.rules
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
-import org.junit.rules.ExternalResource
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.runner.screenshot.Screenshot
+import de.crysxd.octoapp.BuildConfig
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+import timber.log.Timber
+import java.io.File
+import java.util.Locale
 
-open class LazyActivityScenarioRule<A : Activity> : ExternalResource {
+open class LazyActivityScenarioRule<A : Activity>(private val launchActivity: Boolean, private val startActivityIntent: Intent) : TestWatcher() {
 
-    constructor(launchActivity: Boolean, startActivityIntentSupplier: () -> Intent) {
-        this.launchActivity = launchActivity
-        scenarioSupplier = { ActivityScenario.launch(startActivityIntentSupplier()) }
-    }
-
-    constructor(launchActivity: Boolean, startActivityIntent: Intent) {
-        this.launchActivity = launchActivity
-        scenarioSupplier = { ActivityScenario.launch(startActivityIntent) }
-    }
-
-    constructor(launchActivity: Boolean, startActivityClass: Class<A>) {
-        this.launchActivity = launchActivity
-        scenarioSupplier = { ActivityScenario.launch(startActivityClass) }
-    }
-
-    private var launchActivity: Boolean
-
-    private var scenarioSupplier: () -> ActivityScenario<A>
-
+    private val id = String.format(Locale.ROOT, "%x", (0..1024).random())
+    private var scenarioSupplier: () -> ActivityScenario<A> = { ActivityScenario.launch(startActivityIntent) }
     private var scenario: ActivityScenario<A>? = null
 
-
-    override fun before() {
+    override fun starting(description: Description) {
+        super.starting(description)
         if (launchActivity) {
             launch()
         }
+
+        Handler(Looper.getMainLooper()).post {
+            Toast.makeText(InstrumentationRegistry.getInstrumentation().targetContext, description.displayName, Toast.LENGTH_SHORT).show()
+        }
     }
 
-    override fun after() {
+    override fun failed(e: Throwable?, description: Description) {
+        super.failed(e, description)
+        val bitmap = Screenshot.capture().bitmap
+        val file = getScreenshotFile(description)
+        Timber.i("Writing screenshot to ${file.absolutePath}")
+        bitmap.compress(Bitmap.CompressFormat.WEBP_LOSSY, 80, file.outputStream())
+        scenario?.close()
+    }
+
+    override fun succeeded(description: Description) {
+        super.succeeded(description)
         scenario?.close()
     }
 
     fun launch(newIntent: Intent? = null) {
         if ((scenario?.state ?: Lifecycle.State.INITIALIZED) >= Lifecycle.State.CREATED) throw IllegalStateException("Scenario has already been launched!")
-
-        newIntent?.let { scenarioSupplier = { ActivityScenario.launch<A>(it) } }
-
+        newIntent?.let { scenarioSupplier = { ActivityScenario.launch(it) } }
         scenario = scenarioSupplier()
     }
 
     fun getScenario(): ActivityScenario<A> = checkNotNull(scenario)
-}
 
-inline fun <reified A : Activity> lazyActivityScenarioRule(launchActivity: Boolean = true, noinline intentSupplier: () -> Intent): LazyActivityScenarioRule<A> =
-    LazyActivityScenarioRule(launchActivity, intentSupplier)
+    private fun getScreenshotFile(description: Description): File {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        val dir = File(BuildConfig.FAILED_TEST_SCREENSHOT_DIR)
+        if (!dir.exists()) dir.mkdirs()
+        val filename = "${description.testName}.webp"
+        return File(dir, filename)
+    }
 
-inline fun <reified A : Activity> lazyActivityScenarioRule(launchActivity: Boolean = true, intent: Intent? = null): LazyActivityScenarioRule<A> = if (intent == null) {
-    LazyActivityScenarioRule(launchActivity, A::class.java)
-} else {
-    LazyActivityScenarioRule(launchActivity, intent)
+    private val Description.testName get() = "${className.split(".").last()}_${methodName}_$id"
 }
