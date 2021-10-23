210caaa80253e4e10579d80a9c4f47ae44824547
Christian Würthner <c.wurthner@gmail.com>
2021-10-23 15:37:42 +0200

    #905 Fix build

--- feature-file-manager/src/main/java/de/crysxd/octoapp/filemanager/ui/file_details/FileDetailsFragment.kt
+++ feature-file-manager/src/main/java/de/crysxd/octoapp/filemanager/ui/file_details/FileDetailsFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
+import androidx.core.view.updateLayoutParams
 import androidx.core.view.updatePadding
 import androidx.navigation.fragment.navArgs
 import androidx.viewpager2.adapter.FragmentStateAdapter
