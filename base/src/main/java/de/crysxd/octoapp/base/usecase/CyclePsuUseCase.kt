ef471b267c1fc46183b7d5467a9e679b380be675
Christian Würthner <c.wurthner@gmail.com>
2021-10-26 17:23:49 +0200

    #924 Fix PSUControl plugin commands executed twice

--- base/src/main/java/de/crysxd/octoapp/base/usecase/CyclePsuUseCase.kt
+++ base/src/main/java/de/crysxd/octoapp/base/usecase/CyclePsuUseCase.kt
@@ -2,7 +2,6 @@ package de.crysxd.octoapp.base.usecase
 
 import de.crysxd.octoapp.base.OctoAnalytics
 import de.crysxd.octoapp.octoprint.plugins.power.PowerDevice
-import de.crysxd.octoapp.octoprint.plugins.power.runWithPowerPluginFixes
 import kotlinx.coroutines.delay
 import timber.log.Timber
 import javax.inject.Inject
@@ -11,12 +10,8 @@ class CyclePsuUseCase @Inject constructor() : UseCase<PowerDevice, Unit>() {
 
     override suspend fun doExecute(param: PowerDevice, timber: Timber.Tree) {
         OctoAnalytics.logEvent(OctoAnalytics.Event.PsuCycled)
-        runWithPowerPluginFixes(param) {
-            param.turnOff()
-        }
+        param.turnOff()
         delay(2000)
-        runWithPowerPluginFixes(param) {
-            param.turnOn()
-        }
+        param.turnOn()
     }
 }
--- octoprint/src/main/java/de/crysxd/octoapp/octoprint/plugins/power/tradfri/TradfriPowerPlugin.kt
+++ octoprint/src/main/java/de/crysxd/octoapp/octoprint/plugins/power/tradfri/TradfriPowerPlugin.kt
@@ -1,5 +1,6 @@
 package de.crysxd.octoapp.octoprint.plugins.power.tradfri
 
+import de.crysxd.octoapp.octoprint.exceptions.OctoPrintApiException
 import de.crysxd.octoapp.octoprint.models.settings.Settings
 import de.crysxd.octoapp.octoprint.plugins.power.PowerPlugin
 
@@ -8,15 +9,31 @@ class TradfriPowerPlugin(
 ) : PowerPlugin<TradfriPowerDevice> {
 
     internal suspend fun turnOn(device: TradfriPowerDevice) {
-        api.sendCommand(TradfriCommand.TurnDeviceOn(device))
+        runWithFixes {
+            api.sendCommand(TradfriCommand.TurnDeviceOn(device))
+        } ?: Unit
     }
 
     internal suspend fun turnOff(device: TradfriPowerDevice) {
-        api.sendCommand(TradfriCommand.TurnDeviceOff(device))
+        runWithFixes {
+            api.sendCommand(TradfriCommand.TurnDeviceOff(device))
+        }
     }
 
-    internal suspend fun isOn(device: TradfriPowerDevice) =
-        api.sendCommandWithResponse(TradfriCommand.GetDeviceStatus(device)).state == true
+    internal suspend fun isOn(device: TradfriPowerDevice) = runWithFixes {
+        api.sendCommandWithResponse(TradfriCommand.GetDeviceStatus(device)).state
+    } == true
+
+    private suspend fun <T> runWithFixes(block: suspend () -> T): T? = try {
+        block()
+    } catch (e: OctoPrintApiException) {
+        // Tradfri gives 500 on every action...ignore
+        if (e.responseCode != 500) {
+            throw e
+        }
+
+        null
+    }
 
     override fun getDevices(settings: Settings) =
         settings.plugins.values.mapNotNull {
