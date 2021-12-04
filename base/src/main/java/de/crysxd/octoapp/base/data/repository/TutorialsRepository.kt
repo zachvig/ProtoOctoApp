a24fab1db0215da02b959067013e759d824cc481
Christian Würthner <c.wurthner@gmail.com>
2021-12-04 11:28:46 +0100

    #1030 Load playlist from own server

--- base/src/main/java/de/crysxd/octoapp/base/data/repository/TutorialsRepository.kt
+++ base/src/main/java/de/crysxd/octoapp/base/data/repository/TutorialsRepository.kt
@@ -11,14 +11,13 @@ class TutorialsRepository(
 ) {
     companion object {
         private const val MAX_AGE_FOR_NEW_MS = 30 * 24 * 60 * 60 * 1000L
-        const val PLAYLIST_ID = "PL1fjlNqlUKnUuWwB0Jb3wf70wBcF3u-wJ"
     }
 
     private var cached: List<YoutubePlaylist.PlaylistItem>? = null
     private val octoPreferences get() = BaseInjector.get().octoPreferences()
 
     suspend fun getTutorials(skipCache: Boolean) = cached ?: let {
-        val new = dataSource.get(PLAYLIST_ID, skipCache)
+        val new = dataSource.get(skipCache)
         cached = new
         new
     }
