ce9c88a6cd53cb2d330e1e9b2168c6b57696d2ff
Christian Würthner <c.wurthner@gmail.com>
2021-12-28 17:49:44 +0100

    Bump version to 1.13-beta03

--- base-ui/src/main/java/de/crysxd/baseui/common/BottomToolbarView.kt
+++ base-ui/src/main/java/de/crysxd/baseui/common/BottomToolbarView.kt
@@ -15,6 +15,7 @@ import androidx.appcompat.widget.AppCompatImageButton
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.children
+import androidx.core.view.isVisible
 import androidx.core.view.updatePadding
 import androidx.transition.TransitionManager
 import de.crysxd.baseui.R
