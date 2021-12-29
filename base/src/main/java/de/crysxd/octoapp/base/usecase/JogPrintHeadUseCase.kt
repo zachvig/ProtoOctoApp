7ab97c1b3eac71d4100ed9a7ef5e0107d0d145fc
Christian Würthner <c.wurthner@gmail.com>
2021-12-29 16:06:19 +0100

    #1102 #1156 Honor reversed axis options in print profile

--- base/src/main/java/de/crysxd/octoapp/base/usecase/JogPrintHeadUseCase.kt
+++ base/src/main/java/de/crysxd/octoapp/base/usecase/JogPrintHeadUseCase.kt
@@ -1,12 +1,15 @@
 package de.crysxd.octoapp.base.usecase
 
+import de.crysxd.octoapp.base.data.repository.OctoPrintRepository
 import de.crysxd.octoapp.base.network.OctoPrintProvider
 import de.crysxd.octoapp.octoprint.models.printer.PrintHeadCommand
+import de.crysxd.octoapp.octoprint.models.profiles.PrinterProfiles
 import timber.log.Timber
 import javax.inject.Inject
 
 class JogPrintHeadUseCase @Inject constructor(
-    private val octoPrintProvider: OctoPrintProvider
+    private val octoPrintProvider: OctoPrintProvider,
+    private val octoPrintRepository: OctoPrintRepository,
 ) : UseCase<JogPrintHeadUseCase.Param, Unit>() {
 
     data class Param(
@@ -16,13 +19,17 @@ class JogPrintHeadUseCase @Inject constructor(
         val speedMmMin: Int = 4000
     )
 
+    private val PrinterProfiles.Axis?.multiplier get() = if (this?.inverted == true) -1f else 1f
+
     override suspend fun doExecute(param: Param, timber: Timber.Tree) {
+        val axes = octoPrintRepository.getActiveInstanceSnapshot()?.activeProfile?.axes
+
         octoPrintProvider.octoPrint().createPrinterApi().executePrintHeadCommand(
             PrintHeadCommand.JogPrintHeadCommand(
-                param.xDistance,
-                param.yDistance,
-                param.zDistance,
-                param.speedMmMin
+                x = param.xDistance * axes?.x.multiplier,
+                y = param.yDistance * axes?.y.multiplier,
+                z = param.zDistance * axes?.z.multiplier,
+                speed = param.speedMmMin
             )
         )
     }
