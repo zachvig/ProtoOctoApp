148658d1f8bb5bde953138c95a2c6d8aea4f039d
Christian Würthner <c.wurthner@gmail.com>
2022-01-15 13:49:25 +0100

    #916 Fix tests

--- app/src/androidTest/java/de/crysxd/octoapp/framework/BottomToolbarRobot.kt
+++ app/src/androidTest/java/de/crysxd/octoapp/framework/BottomToolbarRobot.kt
@@ -5,14 +5,21 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.swipeRight
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import com.adevinta.android.barista.rule.BaristaRule
+import de.crysxd.octoapp.MainActivity
 import de.crysxd.octoapp.R
 
 object BottomToolbarRobot {
 
-    fun confirmButtonWithSwipe(@IdRes buttonId: Int) {
+    fun confirmButtonWithSwipe(@IdRes buttonId: Int, barista: BaristaRule<MainActivity>) {
+        val controlCenter = barista.activityTestRule.activity.controlCenter
+        val wasEnabled = controlCenter.isEnabled
+        controlCenter.isEnabled = false
         onView(withId(buttonId)).perform(click())
         waitTime(200)
         onView(withId(R.id.swipeButtons)).perform(swipeRight())
+
+        controlCenter.isEnabled = wasEnabled
     }
 
     fun clickButton(@IdRes buttonId: Int) {
